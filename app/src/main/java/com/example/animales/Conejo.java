bbac119989012a2484cc9345f807a75b4420a502
Mimaloro <mlopezt1@ucentral.edu.co>
2023-10-19 21:51:41 -0500

    Conejo

---
+++ app/src/main/java/com/example/animales/Conejo.java
@@ -0,0 +1,27 @@
+package com.example.animales;
+
+public class Conejo {
+
+    String nombre;
+    Double altura;
+    Double peso;
+    String colorPelaje;
+    String genero;
+    String raza;
+    Integer edad;
+
+    public Conejo(String nombre,Double altura,Double peso,String colorPelaje,String genero,String raza,Integer edad){
+
+        this.nombre=nombre;
+        this.altura=altura;
+        this.peso=peso;
+        this.colorPelaje=colorPelaje;
+        this.genero=genero;
+        this.raza=raza;
+        this.edad=edad;
+
+        public String nombre (){
+        }
+
+    }}
+
